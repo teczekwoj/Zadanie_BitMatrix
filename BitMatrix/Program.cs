a854eaecd9f4e233d5b57af188d29c2ce344761c
wojciech.latko <wlkr2000@gmail.com>
2022-05-15 17:56:30 +0200

    Dodaj pliki projektów.

---
+++ BitMatrix/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+ class Program
+    {
+        static void Main(string[] args) //test(main)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
---
+++ BitMatrix_00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitMatrix_00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siema siema, kurwa witam.");
+        }
+    }
+}
